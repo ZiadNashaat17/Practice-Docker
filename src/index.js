b00f4288c6e23ab6670ed4a5a51b9f525688f85a
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-11-12 11:40:41 +0200

    Add MongoDB and Mongoose integration to the application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 8000;
+
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo";
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => console.log("DB connected successfully!"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("<h1>Hello Docker! HI</h1>"));
 
